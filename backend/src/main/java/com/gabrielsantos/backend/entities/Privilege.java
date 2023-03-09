b4784cf1ac73a77784d2410418316fd7dd733e45
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-03-08 23:07:46 -0300

    Classes Category e Privilege criadas

---
+++ backend/src/main/java/com/gabrielsantos/backend/entities/Privilege.java
@@ -0,0 +1,56 @@
+package com.gabrielsantos.backend.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_privilege")
+public class Privilege implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Privilege() {
+    }
+
+    public Privilege(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Privilege privilege = (Privilege) o;
+        return Objects.equals(id, privilege.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
