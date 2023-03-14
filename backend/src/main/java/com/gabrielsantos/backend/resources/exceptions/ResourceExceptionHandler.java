7881c1637b03150b098547ff88dad1ae86e5380a
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-03-14 13:51:32 -0300

    FIX: remoção do campo de message ao retornar uma exceção

--- backend/src/main/java/com/gabrielsantos/backend/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/gabrielsantos/backend/resources/exceptions/ResourceExceptionHandler.java
@@ -18,8 +18,7 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
-        err.setError("Resource not found.");
-        err.setMessage(e.getMessage());
+        err.setError(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
